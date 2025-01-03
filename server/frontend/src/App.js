4afae2fdf3fc7a8a51966a5b01da93fa5e6addd3
andreas-hash <seancrawfordunique@gmail.com>
2025-01-03 11:25:36 -0500

    week 4

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register"; // Import Register component
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} /> {/* Add Register route */}
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
     </Routes>
   );
 }
